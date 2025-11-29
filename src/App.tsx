110c139ff57f18a2ecae0a75e5b49c07f964cfdf
Kotkoa <kotkoa@gmail.com>
2025-11-29 23:35:05 +0100

    feat: move Copyright component to its own file for better organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import './App.css'
 import Typography from '@mui/material/Typography'
-import Link from '@mui/material/Link'
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
-
-function Copyright() {
-  return (
-    <Typography
-      variant="body2"
-      align="center"
-      sx={{
-        color: 'text.secondary',
-      }}
-    >
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  )
-}
+import Copyright from './components/Copyright'
 
 function App() {
   return (
---
+++ src/components/Copyright.tsx
@@ -0,0 +1,22 @@
+import Typography from '@mui/material/Typography'
+import Link from '@mui/material/Link'
+
+function Copyright() {
+  return (
+    <Typography
+      variant="body2"
+      align="center"
+      sx={{
+        color: 'text.secondary',
+      }}
+    >
+      {'Copyright © '}
+      <Link color="inherit" href="https://github.com/Kotkoa">
+        Kotkoa
+      </Link>
+      {new Date().getFullYear()}.
+    </Typography>
+  )
+}
+
+export default Copyright
